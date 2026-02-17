73b9615d9438782cd127c98baeab16c41cd52d47
Saranjith Palakkandy <saranvlmna@gmail.com>
2026-02-17 22:25:37 +0530

    fix

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+import cors from "cors";
 import app from "./app.js";
 import connectDB from "./database/connection.js";
 
 // Connect to database and start server for local development
 await connectDB();
 
-console.log("To test if the deployment is happening or not")
+app.use(cors());
 
 app.listen(4000, "0.0.0.0", () => {
   console.log("Prometheus server is running on port 4000");
