a139b338cc0f43d97cc7c7bfbd43d7e3ea7cfa73
tom-george-tm <tom.george@thoughtminds.io>
2026-02-17 14:00:22 +0000

    Comment out immediate Pub/Sub ACK in gmail webhook and add a deployment test console log.

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import connectDB from "./database/connection.js";
 // Connect to database and start server for local development
 await connectDB();
 
+console.log("To test if the deployment is happening or not")
+
 app.listen(4000, "0.0.0.0", () => {
   console.log("Prometheus server is running on port 4000");
 });
